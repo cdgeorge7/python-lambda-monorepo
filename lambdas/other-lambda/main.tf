8be82c40ba175d4dd3fff3e7b4dabbe86cc0e313
Chris George <cdgeorge7@gmail.com>
2022-06-28 10:26:30 -0500

    Test/tf circle (#6)
    * tf orb test
    
    * test tf
    
    * fix
    
    * good fix...
    
    * lulz
    
    * work???
    
    * ok, gotta work
    
    * fuck fuckity fuck fuck fuck
    
    * lol
    
    * test
    
    * ...
    
    * testing
    
    * aws provider version
    
    * provider block
    
    * change key name

--- lambdas/other-lambda/main.tf
+++ lambdas/other-lambda/main.tf
@@ -7,6 +7,16 @@ terraform {
     dynamodb_table = "terraform-state-example-locks"
     encrypt        = true
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.20.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 module "lambda" {
